b14bc466307c49fac4cdeb97546ab4fdebe6558b
Marishka95s <marishka95@ukr.net>
2021-11-06 14:07:01 +0200

    Swiched to IT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Timer {
 
         this.refs = {
             timerBox: document.querySelector(`${selector}`),
+            spanYears: document.querySelector(`${selector} span[data-value="years"]`),
             spanDays: document.querySelector(`${selector} span[data-value="days"]`),
             spanHours: document.querySelector(`${selector} span[data-value="hours"]`),
             spanMinutes: document.querySelector(`${selector} span[data-value="mins"]`),
@@ -14,24 +15,26 @@ class Timer {
 
     start() {     
         setInterval(() => {
-            const deltaTime = this.targetDate - Date.now();
+            const deltaTime = Date.now() - this.targetDate;
             this.updateClockface(this.getTimeComponents(deltaTime));          
         }, 1000);
     }
 
     getTimeComponents(time) {
-        const days = String(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const years = String(Math.floor(time / (1000 * 60 * 60 * 24 * 365)));
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24))) - years * 365;
         const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-        return { days, hours, mins, secs };
+        return { days, hours, mins, secs, years };
     }
 
     pad(value) {
         return String(value).padStart(2, '0');
     }
 
-    updateClockface({ days, hours, mins, secs }) {
+    updateClockface({ days, hours, mins, secs, years }) {
+        this.refs.spanYears.textContent = years;
         this.refs.spanDays.textContent = days;
         this.refs.spanHours.textContent = hours;
         this.refs.spanMinutes.textContent = mins;
@@ -41,7 +44,7 @@ class Timer {
 
 const CountdownTimer = new Timer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2021'),
+    targetDate: new Date('Oct 30, 2020'),
   });
 
   CountdownTimer.start();
